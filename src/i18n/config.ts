1474f22e4340df7da3bd6ae67f37fc8aacec7cb3
Rodrigo Lima Jaroszewski <rodrigolj@hey.com>
2025-06-25 18:14:03 -0300

    style: unify string quotes and formatting in language configuration

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,15 +1,13 @@
 // Global Language Map
 export const langMap: Record<string, string[]> = {
-  pt: ["pt-BR"],
-  en: ["en-US"],
-};
+  pt: ['pt-BR'],
+}
 
 // Waline Language Map
 // https://waline.js.org/guide/i18n.html
 export const walineLocaleMap: Record<string, string> = {
-  pt: "pt-BR",
-  en: "en-US",
-};
+  pt: 'pt-BR',
+}
 
 // Supported Languages
-export const supportedLangs = Object.keys(langMap).flat();
+export const supportedLangs = Object.keys(langMap).flat()
